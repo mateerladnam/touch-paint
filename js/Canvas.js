a64850f2ab6d0a5a1ca3e820e7bafd9a9b580bf5
Archil Imnadze <archil@imnadze.ge>
2014-07-06 20:23:03 +0400

    simple brush

--- js/Canvas.js
+++ js/Canvas.js
@@ -1,6 +1,10 @@
 function Canvas () {
 
+    var activeTouches = {}
+
     var classPrefix = 'Canvas'
+
+    var brushSize = 4
 
     var size = Math.max(screen.width, screen.height)
 
@@ -8,6 +12,57 @@ function Canvas () {
     canvas.className = classPrefix + '-canvas'
     canvas.width = canvas.height = size
     canvas.style.left = canvas.style.top = -size / 2 + 'px'
+    canvas.addEventListener('touchstart', function (e) {
+        var rect = canvas.getBoundingClientRect()
+        Array.prototype.forEach.call(e.changedTouches, function (touch) {
+            var x = touch.clientX - rect.left,
+                y = touch.clientY - rect.top
+            c.save()
+            c.translate(x, y)
+            c.beginPath()
+            c.arc(0, 0, brushSize / 2, 0, Math.PI * 2)
+            c.fill()
+            c.restore()
+            activeTouches[touch.identifier] = { x: x, y: y }
+        })
+    })
+    canvas.addEventListener('touchmove', function (e) {
+        var rect = canvas.getBoundingClientRect()
+        Array.prototype.forEach.call(e.changedTouches, function (touch) {
+            var activeTouch = activeTouches[touch.identifier]
+            if (activeTouch) {
+                var x = touch.clientX - rect.left,
+                    y = touch.clientY - rect.top
+                c.beginPath()
+                c.moveTo(activeTouch.x, activeTouch.y)
+                c.lineTo(x, y)
+                c.stroke()
+                activeTouch.x = x
+                activeTouch.y = y
+            }
+        })
+    })
+    canvas.addEventListener('touchend', function (e) {
+        var rect = canvas.getBoundingClientRect()
+        Array.prototype.forEach.call(e.changedTouches, function (touch) {
+            var activeTouch = activeTouches[touch.identifier]
+            if (activeTouch) {
+                var x = touch.clientX - rect.left,
+                    y = touch.clientY - rect.top
+                c.save()
+                c.translate(x, y)
+                c.beginPath()
+                c.arc(0, 0, 4, 0, Math.PI * 2)
+                c.fill()
+                c.restore()
+                delete activeTouches[touch.identifier]
+            }
+        })
+    })
+
+    var c = canvas.getContext('2d')
+    c.lineWidth = brushSize * 2
+    c.lineCap = 'round'
 
     var centerElement = Div(classPrefix + '-center')
     centerElement.appendChild(canvas)
---
+++ webapp-manifest/index.php
@@ -0,0 +1,8 @@
+<?php
+
+header('Content-Type: application/x-web-app-manifest+json');
+
+echo json_encode([
+    'name' => 'paint',
+    'description' => 'a',
+]);
