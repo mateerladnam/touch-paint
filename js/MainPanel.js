fa0de1981031fd1a337c162286322d4778554c37
Archil Imnadze <archil@imnadze.ge>
2014-07-07 00:27:05 +0400

    save file without using blobs

--- js/MainPanel.js
+++ js/MainPanel.js
@@ -25,26 +25,14 @@ function MainPanel () {
     })
 
     var saveButton = BarButton('floppy', function () {
-
-        var dataUrl = canvas.canvas.toDataURL('image/png')
-        var base64Data = dataUrl.substring(22)
-        var data = atob(base64Data)
-
-        var part = new Uint8Array(data.length)
-        for (var i = 0; i < data.length; i++) {
-            part[i] = data.charCodeAt(i)
-        }
-
-        var blob = new Blob([part.buffer], { type: 'image/png' })
-        var href = URL.createObjectURL(blob)
-
         var a = document.createElement('a')
-        a.href = href
+        a.href = canvas.canvas.toDataURL('image/png')
         a.download = 'picture.png'
+        a.style.position = 'absolute'
+        a.style.top = a.style.left = 0
         document.body.appendChild(a)
         a.click()
         document.body.removeChild(a)
-
     })
 
     var barElement = Div(classPrefix + '-bar')
