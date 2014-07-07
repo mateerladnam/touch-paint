a51370f87d8b92313cedcad7cf51324c1ad2ea51
Archil Imnadze <archil@imnadze.ge>
2014-07-07 12:33:31 +0000

    fixed red color uncheck

--- js/PalettePanel.js
+++ js/PalettePanel.js
@@ -3,38 +3,51 @@ function PalettePanel (colorListener) {
     var classPrefix = 'PalettePanel'
 
     var blackButton = ColorButton('#000', function () {
+
         redButton.uncheck()
         greenButton.uncheck()
         blueButton.uncheck()
+
         blackButton.check()
         colorListener('#000')
+
     })
     blackButton.addClass(classPrefix + '-blackButton')
     blackButton.check()
 
     var redButton = ColorButton('#f00', function () {
+
         blackButton.uncheck()
         greenButton.uncheck()
         blueButton.uncheck()
+
         redButton.check()
         colorListener('#f00')
+
     })
     redButton.addClass(classPrefix + '-redButton')
 
     var greenButton = ColorButton('#0f0', function () {
+
         blackButton.uncheck()
+        redButton.uncheck()
         blueButton.uncheck()
+
         greenButton.check()
         colorListener('#0f0')
+
     })
     greenButton.addClass(classPrefix + '-greenButton')
 
     var blueButton = ColorButton('#00f', function () {
+
         blackButton.uncheck()
         redButton.uncheck()
         greenButton.uncheck()
+
         blueButton.check()
         colorListener('#00f')
+
     })
     blueButton.addClass(classPrefix + '-blueButton')
 
