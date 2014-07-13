f63145a6a0408e5d3d6d92b5cc27ce475834a330
Archil Imnadze <archil@imnadze.ge>
2014-07-13 20:41:10 +0400

    functionless hue, saturation and luminance sliders

--- js/EditColorPanel.js
+++ js/EditColorPanel.js
@@ -2,7 +2,25 @@ function EditColorPanel () {
 
     var classPrefix = 'EditColorPanel'
 
+    var hueSlider = Slider(0, function () {
+    }, function () {
+    })
+    hueSlider.addClass(classPrefix + '-hueSlider')
+
+    var saturationSlider = Slider(0, function () {
+    }, function () {
+    })
+    saturationSlider.addClass(classPrefix + '-saturationSlider')
+
+    var luminanceSlider = Slider(0, function () {
+    }, function () {
+    })
+    luminanceSlider.addClass(classPrefix + '-luminanceSlider')
+
     var element = Div(classPrefix)
+    element.appendChild(hueSlider.element)
+    element.appendChild(saturationSlider.element)
+    element.appendChild(luminanceSlider.element)
 
     return {
         element: element,
