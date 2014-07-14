d3b92e99aa24120e8345cdc9e9f09351077335fe
Archil Imnadze <archil@imnadze.ge>
2014-07-14 22:29:40 +0400

    icons

--- webapp-manifest/index.php
+++ webapp-manifest/index.php
@@ -3,6 +3,11 @@
 header('Content-Type: application/x-web-app-manifest+json');
 
 echo json_encode([
-    'name' => 'paint',
+    'name' => 'Touch Paint',
     'description' => 'a',
+    'icons' => [
+        '16' => 'sites/paint/images/icons/16.png',
+        '32' => 'sites/paint/images/icons/32.png',
+        '128' => 'sites/paint/images/icons/128.png',
+    ],
 ]);
