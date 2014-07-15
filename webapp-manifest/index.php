23e34bb9c0aaea8cc230a7487270b4f995488f66
Archil Imnadze <archil@imnadze.ge>
2014-07-15 08:10:12 +0000

    all size icons

--- webapp-manifest/index.php
+++ webapp-manifest/index.php
@@ -5,15 +5,18 @@ header('Content-Type: application/x-web-app-manifest+json');
 echo json_encode([
     'name' => 'Touch Paint',
     'description' => 'A program to draw with your finger.',
-    'launch_path' => 'run/',
+    'launch_path' => '/run/',
     'fullscreen' => 'true',
     'developer' => [
         'name' => 'Qliavi Team',
         'url' => 'http://qliavi.com/',
     ],
     'icons' => [
-        '16' => 'images/icons/16.png',
-        '32' => 'images/icons/32.png',
-        '128' => 'images/icons/128.png',
+        '16' => '/images/icons/16.png',
+        '32' => '/images/icons/32.png',
+        '90' => '/images/icons/90.png',
+        '120' => '/images/icons/120.png',
+        '128' => '/images/icons/128.png',
+        '256' => '/images/icons/256.png',
     ],
 ]);
