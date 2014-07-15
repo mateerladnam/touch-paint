8b66827be069a85d768d0d5727229b186f7dde89
Archil Imnadze <archil@imnadze.ge>
2014-07-15 21:26:31 +0400

    limit undo historyto 1024

--- run/js/Canvas.js
+++ run/js/Canvas.js
@@ -22,6 +22,13 @@ function Canvas () {
     var centerElement = Div(classPrefix + '-center')
     centerElement.appendChild(canvas)
 
+    var undoCanvas = document.createElement('canvas')
+    undoCanvas.width = undoCanvas.height = size
+
+    var undoC = undoCanvas.getContext('2d')
+    undoC.fillStyle = '#fff'
+    undoC.fillRect(0, 0, size, size)
+
     var element = Div(classPrefix)
     element.appendChild(centerElement)
 
@@ -36,9 +43,10 @@ function Canvas () {
         operate: function (operation) {
             operations.push(operation)
             operation(c)
+            if (operations.length > 1024) operations.shift()(undoC)
         },
         undo: function () {
-            clear()
+            c.drawImage(undoCanvas, 0, 0)
             if (operations.length) {
                 operations.pop()
                 operations.forEach(function (operation) {
