7cada3066d67bb93f59a605ecf5e1789955449f1
Archil Imnadze <archil@imnadze.ge>
2014-07-16 23:37:24 +0400

    dereference classList in ColorButton

--- run/js/ColorButton.js
+++ run/js/ColorButton.js
@@ -18,15 +18,16 @@ function ColorButton (hue, saturation, luminance, alpha, clickListener) {
     element.addEventListener('touchstart', function (e) {
         e.preventDefault()
         clickListener()
-        element.classList.add('active')
+        classList.add('active')
         clearTimeout(activeTimeout)
         activeTimeout = setTimeout(function () {
-            element.classList.remove('active')
+            classList.remove('active')
         }, 100)
     })
 
     var activeTimeout
     var checked = false
+    var classList = element.classList
 
     setColor(hue, saturation, luminance, alpha)
 
@@ -34,17 +35,17 @@ function ColorButton (hue, saturation, luminance, alpha, clickListener) {
         element: element,
         setColor: setColor,
         addClass: function (className) {
-            element.classList.add(className)
+            classList.add(className)
         },
         check: function () {
-            element.classList.add('checked')
+            classList.add('checked')
             checked = true
         },
         isChecked: function () {
             return checked
         },
         uncheck: function () {
-            element.classList.remove('checked')
+            classList.remove('checked')
             checked = false
         },
     }
