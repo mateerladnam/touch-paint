413f46b0facce82dc4a4ebf4864d132fcc7e159f
Archil Imnadze <archil@imnadze.ge>
2014-07-16 22:54:06 +0400

    brush size preview

--- run/combined.js
+++ run/combined.js
@@ -965,22 +965,43 @@ function PalettePanel (colorListener, closeListener) {
 ;
 function ParamsPanel (brushSize, changeListener, closeListener) {
 
+    function updatePreview () {
+        previewC.save()
+        previewC.clearRect(0, 0, previewCanvas.width, previewCanvas.height)
+        previewC.translate(previewCanvas.width / 2, previewCanvas.height / 2)
+        previewC.beginPath()
+        previewC.arc(0, 0, (brushSize + 1) / 2, 0, Math.PI * 2)
+        previewC.fill()
+        previewC.restore()
+    }
+
     var classPrefix = 'ParamsPanel'
 
     var minBrushSize = 1,
-        maxBrushSize = 32,
+        maxBrushSize = 48,
         initialRatio = (brushSize - minBrushSize) / (maxBrushSize - minBrushSize)
 
+    var previewCanvas = document.createElement('canvas')
+    previewCanvas.width = previewCanvas.height = maxBrushSize + 4
+    previewCanvas.className = classPrefix + '-previewCanvas'
+
+    var previewC = previewCanvas.getContext('2d')
+
     var slider = Slider(initialRatio, function (ratio) {
-        changeListener(minBrushSize + ratio * maxBrushSize)
+        brushSize = minBrushSize + ratio * maxBrushSize
+        changeListener(brushSize)
+        updatePreview()
     }, closeListener)
     slider.addClass(classPrefix + '-slider')
 
     var contentElement = Div(classPrefix + '-content')
     contentElement.appendChild(slider.element)
+    contentElement.appendChild(previewCanvas)
 
     var element = Div(classPrefix)
     element.appendChild(contentElement)
+
+    updatePreview()
 
     return {
         element: element,
--- run/index.php
+++ run/index.php
@@ -2,6 +2,6 @@
 
 include_once 'fns/echo_html.php';
 echo_html(
-    '<link rel="stylesheet" type="text/css" href="compressed.css?4" />',
-    '<script type="text/javascript" src="compressed.js?7"></script>'
+    '<link rel="stylesheet" type="text/css" href="compressed.css?5" />',
+    '<script type="text/javascript" src="compressed.js?8"></script>'
 );
