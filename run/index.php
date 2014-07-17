c984d8e6f637310b4671f44a972ac561ee8036df
Archil Imnadze <archil@imnadze.ge>
2014-07-17 14:19:42 +0000

    load bar

--- run/index.php
+++ run/index.php
@@ -2,6 +2,6 @@
 
 include_once 'fns/echo_html.php';
 echo_html(
-    '<link rel="stylesheet" type="text/css" href="compressed.css?7" />',
-    '<script type="text/javascript" src="compressed.js?10"></script>'
+    '<link rel="stylesheet" type="text/css" href="compressed.css?8" />',
+    '<script type="text/javascript" src="compressed.js?11"></script>'
 );
--- run/js/Main.js
+++ run/js/Main.js
@@ -1,4 +1,26 @@
 (function () {
-    var mainPanel = MainPanel()
-    document.body.appendChild(mainPanel.element)
+
+    var progressElement = Div('Main-progress')
+
+    var loadBarElement = Div('Main-loadBar')
+    loadBarElement.appendChild(progressElement)
+
+    document.body.appendChild(loadBarElement)
+
+    var finished = 0
+    var icons = ['pencil', 'eraser', 'palette', 'params', 'undo', 'burger']
+    icons.forEach(function (icon) {
+        var image = new Image
+        image.src = 'images/' + icon + '.svg'
+        image.onload = image.onerror = image.onabort = function () {
+            finished++
+            progressElement.style.width = finished / icons.length * 100 + '%'
+            if (finished == icons.length - 1) {
+                var mainPanel = MainPanel()
+                document.body.removeChild(loadBarElement)
+                document.body.appendChild(mainPanel.element)
+            }
+        }
+    })
+
 })()
