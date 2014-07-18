2d64a81cacdd709823e63dcd741f667dfee047bf
Archil Imnadze <archil@imnadze.ge>
2014-07-18 12:24:56 +0400

    fixed compressed.js cache problem

--- run/index.php
+++ run/index.php
@@ -5,5 +5,5 @@ include_once 'fns/get_revision.php';
 echo_html(
     '<html manifest="cache-manifest/">',
     '<link rel="stylesheet" type="text/css" href="compressed.css?'.get_revision('compressed.css').'" />',
-    '<script type="text/javascript" src="compressed.js?'.get_revision('compressed.css').'"></script>'
+    '<script type="text/javascript" src="compressed.js?'.get_revision('compressed.js').'"></script>'
 );
