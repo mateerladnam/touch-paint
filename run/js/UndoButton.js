477f3d20f251429099e71f31b0b4e83c974efa27
Archil Imnadze <archil@imnadze.ge>
2014-07-21 07:32:48 +0000

    support mouse on undo button

--- run/js/UndoButton.js
+++ run/js/UndoButton.js
@@ -1,35 +1,59 @@
 function UndoButton (undoListener) {
 
+    function beginUndo () {
+
+        function endUndo () {
+            removeEventListener('mouseup', mouseUp)
+            removeEventListener('touchend', touchEnd)
+            classList.remove('active')
+            clearInterval(repeatInterval)
+        }
+
+        function mouseUp () {
+            if (touched) touched = false
+            else endUndo()
+        }
+
+        function touchEnd (e) {
+            touched = true
+            var touches = e.changedTouches
+            for (var i = 0; i < touches.length; i++) {
+                if (touches[i].identifier === identifier) {
+                    identifier = null
+                    endUndo()
+                }
+            }
+        }
+
+        var touched = false
+
+        addEventListener('touchend', touchEnd)
+        addEventListener('mouseup', mouseUp)
+        classList.add('active')
+        repeatInterval = setInterval(undoListener, 50)
+        undoListener()
+
+    }
+
     var contentElement = Div('Button-content')
     contentElement.style.backgroundImage = 'url(images/undo.svg)'
 
     var element = Div('Button UndoButton disabled')
     element.appendChild(contentElement)
+    element.addEventListener('mousedown', function (e) {
+        if (touched) touched = false
+        else beginUndo()
+    })
     element.addEventListener('touchstart', function (e) {
-
-        function touchEnd (e) {
-            var touches = e.changedTouches
-            for (var i = 0; i < touches.length; i++) {
-                if (touches[i].identifier === identifier) {
-                    identifier = null
-                    classList.remove('active')
-                    removeEventListener('touchend', touchEnd)
-                    clearInterval(repeatInterval)
-                }
-            }
-        }
-
+        touched = true
         if (identifier === null) {
             e.preventDefault()
             identifier = e.changedTouches[0].identifier
-            classList.add('active')
-            addEventListener('touchend', touchEnd)
-            repeatInterval = setInterval(undoListener, 50)
-            undoListener()
+            beginUndo()
         }
-
     })
 
+    var touched = false
     var identifier = null
     var repeatInterval
     var classList = element.classList
